db6da0874c4ce5e7571f7bf8b8c7922dc4325f06
amirNazeh <130168914+amirNazeh@users.noreply.github.com>
2023-08-14 03:07:03 +0300

    Add files via upload

--- src/Components/featured-ads/featured-ads.js
+++ src/Components/featured-ads/featured-ads.js
@@ -10,7 +10,7 @@ const FeaturedAds = () => {
     <>
       <Row>
         <Col lg={3}>
-          <div className="explantionOfUse d-lg-block d-md-none ">
+          <div className="explantionOfUse d-lg-block d-none ">
             <h2 className="spanClass m-2 my-5 py-3">
               {strings.howToUseTheSouqCar}
             </h2>
